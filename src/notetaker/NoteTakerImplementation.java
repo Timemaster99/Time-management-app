03aef6d04bc8a94a60515eb58117e3df9d4dbdcb
Timemaster99 <elijahrobot@gmail.com>
2023-09-19 18:55:40 -0400

    Adjusted try catch statements in NoteTakerImplementation

--- src/notetaker/NoteTakerImplementation.java
+++ src/notetaker/NoteTakerImplementation.java
@@ -75,39 +75,39 @@ public class NoteTakerImplementation implements NoteTaker {
 	
 	public void addNote(String title, String body, int category) throws SQLException {
 		try {
-		    Class.forName("org.postgresql.Driver");
+			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		// Attempt connection
-		Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-		System.out.println("Connection established");
+		try {
+			// Attempt connection
+			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+			System.out.println("Connection established");
 
-		// String for query
-		String sqlQuery = "INSERT INTO notes (title, body, category_id) VALUES (?, ?, ?)";
-		// Prepare statement
-		PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
-		preparedStatement.setString(1, title);
-		preparedStatement.setString(2, body);
-		preparedStatement.setInt(3, category);
-		
-		// Check if insertion is successful
-		int rowsAffected = preparedStatement.executeUpdate();
-		if (rowsAffected > 0) {
-			System.out.println("Insertion successful!");
-		} else {
-			System.out.println("Insertion failed!");
+			// String for query
+			String sqlQuery = "INSERT INTO notes (title, body, category_id) VALUES (?, ?, ?)";
+			// Prepare statement
+			PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+			preparedStatement.setString(1, title);
+			preparedStatement.setString(2, body);
+			preparedStatement.setInt(3, category);
+
+			// Check if insertion is successful
+			int rowsAffected = preparedStatement.executeUpdate();
+			if (rowsAffected > 0) {
+				System.out.println("Insertion successful!");
+			} else {
+				System.out.println("Insertion failed!");
+			}
+
+			// Closing connection
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
-		// Closing connection
-		if (connection != null) {
-		    try {
-		        connection.close();
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		    }
-		}
-		
+
 	}
 
 	public void deleteCategory(String title) {
@@ -124,90 +124,93 @@ public class NoteTakerImplementation implements NoteTaker {
 
 	public Note getNote(int id) throws SQLException {
 		try {
-		    Class.forName("org.postgresql.Driver");
+			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		// Attempt connection
-		Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-		System.out.println("Connection established");
+		try {
+			// Attempt connection
+			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+			System.out.println("Connection established");
 
-		// String for query
-		String sqlQuery = "SELECT * FROM notes WHERE id = ?";
-		// Prepare statement
-		PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
-		preparedStatement.setInt(1, id);
-		
-		ResultSet resultSet = preparedStatement.executeQuery();
-		Note note = null;
-		
-	    while (resultSet.next()) {
-	        // Process the results
-	        int tempid = resultSet.getInt("id");
-	        String title = resultSet.getString("title");
-	        String body = resultSet.getString("body");
-	        int category_id = resultSet.getInt("category_id");
-	        Timestamp createdTime = resultSet.getTimestamp("createdTime");
-	        // Do something with the retrieved data
-	        
-	        // TODO: Find a way to feed this a category object
-	        //Note retrievedNote = new Note(tempid, title, body, category_id, createdTime);
-	        note = new Note(tempid, title, body, getCategory(category_id), createdTime);
-	    }
-		
-		// Closing connection
-		if (connection != null) {
-		    try {
-		        connection.close();
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		    }
+			// String for query
+			String sqlQuery = "SELECT * FROM notes WHERE id = ?";
+			// Prepare statement
+			PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+			preparedStatement.setInt(1, id);
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+			Note note = null;
+
+			while (resultSet.next()) {
+				// Process the results
+				int tempid = resultSet.getInt("id");
+				String title = resultSet.getString("title");
+				String body = resultSet.getString("body");
+				int category_id = resultSet.getInt("category_id");
+				Timestamp createdTime = resultSet.getTimestamp("createdTime");
+				// Do something with the retrieved data
+
+				// TODO: Find a way to feed this a category object
+				//Note retrievedNote = new Note(tempid, title, body, category_id, createdTime);
+				note = new Note(tempid, title, body, getCategory(category_id), createdTime);
+			}
+
+			// Closing connection
+			if (connection != null) {
+				connection.close();
+			}
+
+			return note;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
 		}
-		
-		return note;
 	}
 
 	@Override
 	public NoteCategory getCategory(int id) throws SQLException {
 		try {
-		    Class.forName("org.postgresql.Driver");
+			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		// Attempt connection
-		Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-		System.out.println("Connection established");
+		try {
+			// Attempt connection
+			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+			System.out.println("Connection established");
 
-		// String for query
-		String sqlQuery = "SELECT * FROM categories WHERE id = ?";
-		// Prepare statement
-		PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
-		preparedStatement.setInt(1, id);
-		
-		ResultSet resultSet = preparedStatement.executeQuery();
-		NoteCategory category = null;
-		
-	    while (resultSet.next()) {
-	        // Process the results
-	        int tempid = resultSet.getInt("id");
-	        String title = resultSet.getString("title");
-	        // Do something with the retrieved data
-	        
-	        // TODO: create category object to return
-	        category = new NoteCategory(tempid, title);
-	    }
-		
-		// Closing connection
-		if (connection != null) {
-		    try {
-		        connection.close();
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		    }
+			// String for query
+			String sqlQuery = "SELECT * FROM categories WHERE id = ?";
+			// Prepare statement
+			PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+			preparedStatement.setInt(1, id);
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+			NoteCategory category = null;
+
+			while (resultSet.next()) {
+				// Process the results
+				int tempid = resultSet.getInt("id");
+				String title = resultSet.getString("title");
+				// Do something with the retrieved data
+
+				// TODO: create category object to return
+				category = new NoteCategory(tempid, title);
+			}
+
+			// Closing connection
+			if (connection != null) {
+				connection.close();
+			}
+
+			return category;
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
 		}
-		
-		return category;
-		
+
+
 	}
-	
+
 }
